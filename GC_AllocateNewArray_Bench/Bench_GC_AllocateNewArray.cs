ca510f78d6fc61856e85bea65054da1baf8e796c
Alexander Radchenko <radchenkosasha@gmail.com>
2023-07-13 07:17:15 +0700

    GC_AllocateNewArray_Bench

---
+++ GC_AllocateNewArray_Bench/Bench_GC_AllocateNewArray.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace GC_AllocateNewArray_Bench;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.Declared)]
+public class BenchFromSpan
+{
+    //[Params(0, 1, 5)]
+    //[Params(0, 1, 5, 10, 100, 1_000)]
+    //[Params(0, 1, 5, 10, 100, 1_000, 10_000)]
+    //[Params(0, 1, 5, 10, 100, 1_000, 10_000, 100_000)]
+    [Params(0, 1, 5, 10, 100, 1_000, 10_000, 100_000, 1_000_000)]
+    //[Params(0, 1, 5, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000)]
+    public int Count { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public void Array_Int_Create()
+    {
+        _ = new int[Count];
+    }
+
+    [Benchmark]
+    public void Array_Int_Allocate()
+    {
+        _ = GC.AllocateArray<int>(Count);
+    }
+
+    [Benchmark]
+    public void Array_Int_AllocateUninitialized()
+    {
+        _ = GC.AllocateUninitializedArray<int>(Count);
+    }
+}
---
+++ GC_AllocateNewArray_Bench/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+_ = BenchmarkRunner.Run(
+    typeof(Program).Assembly,
+    ManualConfig.Create(DefaultConfig.Instance)
+        .WithOptions(ConfigOptions.JoinSummary),
+    args);
+
